f1e894a8710b965601273d3177fda534d2768374
Benjamin Stein <ben@liatrio.com>
2019-03-21 10:48:04 -0400

    adding application and controller for adding, reading, and deleting posts

--- src/main/java/io/liatr/sampleappapi/HelloController.java
+++ src/main/java/io/liatr/sampleappapi/HelloController.java
@@ -1,10 +1,6 @@
 package io.liatr.sampleappapi;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
